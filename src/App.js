64d4b21229e4f73e338dc26a193d3746aceb7f1e
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-07-27 02:07:48 -0300

    added actions, setting redux up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper'
-import AppBar from '@material-ui/core/AppBar'
-import Typography from '@material-ui/core/Typography'
-import Toolbar from '@material-ui/core/Toolbar'
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 import './App.css';
 import { Grid, Col, Row } from 'react-flexbox-grid';
-import LocationList from './components/locationList';
-import ForecastExtended from './components/ForecastExtended'
+import LocationListContainer from './containers/LocationListContainer';
+import ForecastExtended from './components/ForecastExtended';
 
 const cities = [
   'Buenos Aires',
+  'La Plata',
   'Bahia Blanca',
   'Londres',
   'Tokyo'
 ]
+
 class App extends Component{
 
   constructor(){
@@ -22,10 +24,6 @@ class App extends Component{
 
     this.state = {city: null}
   }
-
-  handleSelectedLocation = city => {
-    this.setState({ city });
-  } 
 
   render() {
     const { city } = this.state
@@ -43,11 +41,10 @@ class App extends Component{
         <Row>
           <Col xs={12} md={6}>
 
-          <LocationList cities={cities} 
-          onSelectedLocation={this.handleSelectedLocation}/>
+          <LocationListContainer cities={cities}/>
           </Col>
 
-          <Col s={12} md={6}>
+          <Col xs={12} md={6}>
             <Paper elevation={4}>
               <div className='details'>
                 {
@@ -65,4 +62,7 @@ class App extends Component{
   }
 }
 
+
+
 export default App;
+
--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
 import ForecastItem from './ForecastItem';
-import forecastExtended from '../services/transformForecast'
 import transformForecast from '../services/transformForecast';
 
 
